794e133230b9b418a1600b0f628027926fc2337e
Dao Duong <dao.duong@saritasa.com>
2022-12-07 16:19:21 +0700

    Complete day 6

---
+++ 2022/day6/a.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+fs.readFile("./input.txt", "utf8", (err, data) => {
+    const input = data.split('')
+    let ans = 0; 
+    const numberOfCharacter = 3;
+    const haveDuplicate = (arr) => {
+        return arr.some((item, index) => arr.indexOf(item) !== index);
+    }
+    for (let i = numberOfCharacter; i < input.length; i++) {
+        const arr = input.slice(i - numberOfCharacter, i)
+        if (!haveDuplicate(arr)) {
+            ans = i;
+            break
+        }
+    }
+    console.log(ans)
+});
