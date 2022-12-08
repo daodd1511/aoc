dfdf80425dfee47471ea76c5e604ecf318579abf
Dao Duong <dao.duong@saritasa.com>
2022-12-08 16:18:25 +0700

    Refactor day 1

--- 2022/day1/a.js
+++ 2022/day1/a.js
@@ -18,30 +18,3 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
   console.log(sum)
   
 });
-
-// Advent of code 2022 day 1
-
-// Path: b.js
-
-// const fs = require('fs');
-
-// fs.readFile('./input.txt', 'utf8', (err, data) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   const nums = data.split('
-
-// ').map(Number);
-//   const target = 2020;
-//   for (let i = 0; i < nums.length; i++) {
-//     for (let j = i + 1; j < nums.length; j++) {
-//       for (let k = j + 1; k < nums.length; k++) {
-//         if (nums[i] + nums[j] + nums[k] === target) {
-//           console.log(nums[i] * nums[j] * nums[k]);
-//           return;
-//         }
-//       }
-//     }
-//   }
-// });
