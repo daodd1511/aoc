f34da5d2bb2932bbc94f10a8788262b714203b7b
Thomas Duong <thomasduong1511@gmail.com>
2025-02-18 15:02:47 +0700

    Complete day 9 part 1

---
+++ 2024/day9/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fileUtils"
+	"fmt"
+	"strconv"
+)
+
+type DiskMap struct {
+	amount      int
+	id          int
+	freeSpace   int
+	movedBlocks []int
+}
+
+func main() {
+	input, err := fileUtils.ReadFileAsLines("./input.txt")
+	// input, err := fileUtils.ReadFileAsLines("./example.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	solve(input)
+}
+
+func solve(input []string) {
+	diskMap := []DiskMap{}
+	idCount := 0
+	emptySpaces := 0
+	for pos, char := range input[0] {
+		if pos%2 == 0 {
+			current, _ := strconv.Atoi(string(char))
+			diskMap = append(diskMap, DiskMap{
+				amount:    current,
+				id:        idCount,
+				freeSpace: 0,
+			})
+
+			idCount++
+		} else {
+			current, _ := strconv.Atoi(string(char))
+			diskMap[len(diskMap)-1].freeSpace += current
+			emptySpaces += current
+		}
+	}
+
+	pendingMoveBlock := []int{len(diskMap) - 1, 0}
+	tempAmount := 0
+	for i := 0; i < len(diskMap); i++ {
+		for j := 0; j < diskMap[i].freeSpace; j++ {
+			if pendingMoveBlock[1] >= diskMap[pendingMoveBlock[0]].amount {
+				diskMap = removeValue(diskMap, diskMap[pendingMoveBlock[0]])
+				pendingMoveBlock[0]--
+				pendingMoveBlock[1] = 1
+				tempAmount = diskMap[pendingMoveBlock[0]].amount
+			} else {
+				pendingMoveBlock[1]++
+				tempAmount--
+			}
+			if i >= len(diskMap)-1 {
+				break
+			}
+			moveBlock := diskMap[pendingMoveBlock[0]]
+			if moveBlock.id != diskMap[i].id {
+				diskMap[i].movedBlocks = append(diskMap[i].movedBlocks, moveBlock.id)
+			}
+		}
+	}
+	diskMap[len(diskMap)-1].amount = tempAmount
+
+	result := 0
+	index := 0
+	for i := 0; i < len(diskMap); i++ {
+		for j := 0; j < diskMap[i].amount; j++ {
+			result += index * diskMap[i].id
+			index++
+		}
+
+		for k := 0; k < len(diskMap[i].movedBlocks); k++ {
+			result += index * diskMap[i].movedBlocks[k]
+			index++
+		}
+	}
+	print(result)
+}
+
+func print(input ...any) {
+	fmt.Println(input...)
+}
+
+func removeValue(slice []DiskMap, value DiskMap) []DiskMap {
+	for i, v := range slice {
+		if v.id == value.id {
+			return append(slice[:i], slice[i+1:]...) // Remove element
+		}
+	}
+	return slice // Return unchanged if value is not found
+}
